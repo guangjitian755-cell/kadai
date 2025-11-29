42bf1f5dd365cc2164c19922f8319b7e587874d2
Hikaru Yoshida <guangjitian755@gmail.com>
2025-11-30 06:47:57 +0900

    テスト実装

--- test.bash
+++ test.bash
@@ -2,29 +2,45 @@
 # SPDX-FileCopyrightText: 2025 Hikaru Yoshida
 # SPDX-License-Identifier: BSD-3-Clause
 
+#!/bin/bash
+
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo "${1}行目が違う"
+    res=1
 }
 
 res=0
 
 #NOMAL INPUT
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(echo -e "2 1 1\n1 2 1\n0 1 2" | ./inverse)
+expected=$'0.600 -0.400 0.200\n-0.200 0.800 -0.400\n-0.200 -0.200 0.600'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
-#STRANGE INPUT
-out=$(echo あ | ./plus)
+# STRANGE INPUT 
+out=$(echo -e "1 2 3\n4 5 6\n7 8 9" | ./inverse)
+[ "${out}" = "逆行列は存在しない" ] || ng "$LINENO"
+
+out=$(echo -e "1 2\n1 1" | ./inverse)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo a | ./plus)
+out=$(echo 1 | ./inverse)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo  | ./plus)
+out=$(echo あ | ./inverse)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-[ "$res" = 0 ] && echo OK
+out=$(echo a | ./inverse)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./inverse)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+# --- 結果表示 ---
+[ "$res" = 0 ] && echo "OK"
 exit $res
+
